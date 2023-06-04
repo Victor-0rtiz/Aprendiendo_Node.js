93a57811208556715aa24f3656f09ca49cef7485
Victor Oss <vicjosneros@gmail.com>
2023-06-03 23:49:21 -0600

    Prueba con promesas en cadena con async await

--- pruebaPM.js
+++ pruebaPM.js
@@ -19,15 +19,28 @@ const function2 = () => {
     })
 }
 
+const function3 = async () => {
+    const respuesta = await function1();
+    const respuesta2 = await function2();
+    console.log(respuesta, " desde await");
+    console.log(respuesta2, " desde await");
+}
+
+
+
 
 
 
 function1()
     .then(resp => {
         console.log(resp)
-        return function2(resp)
+        return function2;
     })
     .then(resp2 => {
         console.log(resp2);
     })
+    .catch(err => console.log(err));
+
+
+function3()
     .catch(err => console.log(err))
